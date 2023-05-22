c72d65dc692f9666aef14b164db382e301479d69
Gustavo Chavarro Ortiz <gchavarro@cvent.com>
2023-05-21 22:54:27 -0400

    removed delivered attribute

--- src/main/java/com/local/DroneCodingTest/entities/Location.java
+++ src/main/java/com/local/DroneCodingTest/entities/Location.java
@@ -3,7 +3,6 @@ package com.local.DroneCodingTest.entities;
 public class Location {
     private String name;
     private int weight;
-    private boolean delivered;
 
     public Location(String name, int weight) {
         this.name = name;
@@ -24,13 +23,5 @@ public class Location {
 
     public void setWeight(int weight) {
         this.weight = weight;
-    }
-
-    public boolean isDelivered() {
-        return delivered;
-    }
-
-    public void setDelivered(boolean delivered) {
-        this.delivered = delivered;
     }
 }
